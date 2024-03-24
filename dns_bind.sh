8454724d87ea97f93d3c0aedf35e9b9e33971a20
kasei0 <clr_kasei@icloud.com>
2024-03-24 13:47:43 +0800

    Update dns_bind.sh

--- dns_bind.sh
+++ dns_bind.sh
@@ -1,36 +1,28 @@
 #!/usr/bin/env sh
 
-# This script adds a TXT record to a BIND DNS zone file.
-# NOTE: This script assumes that the zone file is named after the domain
+# This script adds a TXT record to a BIND DNS zone file for using acme.sh, if you set up 
+# an authority dns server for your domain.
+# NOTE: This script assumes that the zone file is named after the db. like db.domain.com
 # and is located in /etc/bind/zones/. Ensure the BIND configuration is set up accordingly.
-
+# If you enabled dnssec function, you should change dir and name of ZSK and KSK below in 
+# resign_zone function, and add it into dns_bind_add and dns_bind_rm manully.
 
 #returns 0 means success, otherwise error.
 #
-#Author: Neilpang
+#Template Author: Neilpang
 #Report Bugs here: https://github.com/acmesh-official/acme.sh
 #
-########  Public functions #####################
+########  functions start #####################
 
 # Please Read this guide first: https://github.com/acmesh-official/acme.sh/wiki/DNS-API-Dev-Guide
+# Usage: dns_myapi_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
 
-# Utility functions
-#_info() {
-#    printf "\\033[1;32m%s\\033[0m\\n" "$@"
-#}
 
-#_debug() {
-#    _info "$1: $2"
-#}
 
-#_err() {
-#    printf "\\033[1;31m%s\\033[0m\\n" "$@" >&2
-#}
-
+# please modify dir for yours.
 ZONE_FILES_DIR="/etc/bind/zones"
-RELOAD_CMD="sudo systemctl reload bind9"
 
-#Usage: dns_myapi_add   _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+RELOAD_CMD="sudo systemctl reload bind9"
 
 # Extracts the root domain from the full domain
 # Example: _extract_root_domain "_acme-challenge.www.example.com" returns "example.com"
@@ -38,6 +30,16 @@ _extract_root_domain() {
     echo "$1" | rev | cut -d. -f1-2 | rev
 }
 
+# Using dnssec function with bind9 need resign zone file after added or removed record.
+# You should change dir and name of ZSK and KSK below as your own before add this 
+# function into dns_bind_add and dns_bind_rm.
+resign_zone(){
+    echo "Re-signing the zone: ${zone_file}"
+    dnssec-signzone -K ${ZONE_FILES_DIR} -o ${root_domain} -k K${root_domain}.${ZSK}.key $zone_file K${root_domain}.${KSK}.key
+    $RELOAD_CMD
+}
+
+#Add the txt record before validation.
 dns_bind_add() {
   fulldomain=$1
   txtvalue=$2
@@ -68,7 +70,6 @@ dns_bind_add() {
   fi
 }
 
-#Usage: fulldomain txtvalue
 #Remove the txt record after validation.
 dns_bind_rm() {
   fulldomain=$1
@@ -92,4 +93,4 @@ dns_bind_rm() {
   _info "BIND reloaded successfully." || _err "Failed to reload BIND."
 }
 
-####################  Private functions below ##################################
+########  functions end #####################
